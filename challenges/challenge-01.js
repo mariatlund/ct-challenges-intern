7236d7a11a7f05cc44b20ab34d4480b31aa22cb7
Daniel Schmidt <dsc@charlietango.dk>
2022-05-18 17:19:37 +0200

    use inline test source

--- challenges/challenge-01.js
+++ challenges/challenge-01.js
@@ -1,3 +1,5 @@
+import { expect, test } from "vitest";
+
 /**
  * Given a 3 letter weekday, convert it to a full length.
  * E.g. : `mon` => `Monday`
@@ -7,5 +9,32 @@
  **/
 export function convertToWeekDay(shortWeekDay) {
   // Add code here ‍💻
-  return 'Monday'
+  return "Monday";
+}
+
+/*****************************************/
+/* Test Code Below                       */
+/* No need to touch this 🙈              */
+/*****************************************/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+
+if (import.meta.vitest) {
+  test("should set names correctly", () => {
+    expect(convertToWeekDay("mon")).toMatch(/monday/i);
+    expect(convertToWeekDay("tue")).toMatch(/tuesday/i);
+    expect(convertToWeekDay("wed")).toMatch(/wednesday/i);
+    expect(convertToWeekDay("thu")).toMatch(/thursday/i);
+    expect(convertToWeekDay("fri")).toMatch(/friday/i);
+    expect(convertToWeekDay("sau")).toMatch(/saturday/i);
+    expect(convertToWeekDay("sun")).toMatch(/sunday/i);
+    expect(convertToWeekDay("invalid")).toBeFalsy();
+  });
 }
--- challenges/challenge-02.js
+++ challenges/challenge-02.js
@@ -1,3 +1,5 @@
+import { expect, test } from "vitest";
+
 /**
  * Given a time in the format `hh:mm`, for start and end, calculate the total duration between them.
  *
@@ -7,4 +9,32 @@
  **/
 export function durationInHours(start, end) {
   // Add code here ‍💻
+}
+
+/*****************************************/
+/* Test Code Below                       */
+/* No need to touch this 🙈              */
+/*****************************************/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+/**/
+
+if (import.meta.vitest) {
+  test("should calculate duration", () => {
+    expect(durationInHours("00:00", "03:00")).toBeCloseTo(3);
+    expect(durationInHours("02:00", "04:00")).toBeCloseTo(2);
+    expect(durationInHours("04:00", "08:00")).toBeCloseTo(4);
+  });
+
+  test("should calculate duration with minutes", () => {
+    expect(durationInHours("08:30", "10:00")).toBeCloseTo(1.5);
+    expect(durationInHours("10:30", "12:45")).toBeCloseTo(2.25);
+    expect(durationInHours("12:45", "14:30")).toBeCloseTo(1.75);
+  });
 }
