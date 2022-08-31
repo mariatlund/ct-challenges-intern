9621fdb8fbf0598c617c22b396d7f9c446316ea6
malte <malt0533@stud.kea.dk>
2022-08-31 14:03:16 +0200

    test fixes

--- __tests__/challenge-01.test.js
+++ __tests__/challenge-01.test.js
@@ -2,12 +2,12 @@ import { expect, test } from "vitest";
 import { convertToWeekDay } from "../challenges/challenge-01";
 
 test("should set names correctly", () => {
-  expect(convertToWeekDay("mon")).toMatch(/monday/i);
-  expect(convertToWeekDay("tue")).toMatch(/tuesday/i);
-  expect(convertToWeekDay("wed")).toMatch(/wednesday/i);
-  expect(convertToWeekDay("thu")).toMatch(/thursday/i);
-  expect(convertToWeekDay("fri")).toMatch(/friday/i);
-  expect(convertToWeekDay("sau")).toMatch(/saturday/i);
-  expect(convertToWeekDay("sun")).toMatch(/sunday/i);
-  expect(convertToWeekDay("invalid")).toBeFalsy();
+	expect(convertToWeekDay("mon")).toMatch(/monday/i);
+	expect(convertToWeekDay("tue")).toMatch(/tuesday/i);
+	expect(convertToWeekDay("wed")).toMatch(/wednesday/i);
+	expect(convertToWeekDay("thu")).toMatch(/thursday/i);
+	expect(convertToWeekDay("fri")).toMatch(/friday/i);
+	expect(convertToWeekDay("sat")).toMatch(/saturday/i);
+	expect(convertToWeekDay("sun")).toMatch(/sunday/i);
+	expect(convertToWeekDay("invalid")).toBeFalsy();
 });
--- __tests__/challenge-02.test.js
+++ __tests__/challenge-02.test.js
@@ -1,13 +1,14 @@
 import { expect, test } from "vitest";
+import { durationInHours } from "../challenges/challenge-02";
 
 test("should calculate duration", () => {
-  expect(durationInHours("00:00", "03:00")).toBeCloseTo(3);
-  expect(durationInHours("02:00", "04:00")).toBeCloseTo(2);
-  expect(durationInHours("04:00", "08:00")).toBeCloseTo(4);
+	expect(durationInHours("00:00", "03:00")).toBeCloseTo(3);
+	expect(durationInHours("02:00", "04:00")).toBeCloseTo(2);
+	expect(durationInHours("04:00", "08:00")).toBeCloseTo(4);
 });
 
 test("should calculate duration with minutes", () => {
-  expect(durationInHours("08:30", "10:00")).toBeCloseTo(1.5);
-  expect(durationInHours("10:30", "12:45")).toBeCloseTo(2.25);
-  expect(durationInHours("12:45", "14:30")).toBeCloseTo(1.75);
+	expect(durationInHours("08:30", "10:00")).toBeCloseTo(1.5);
+	expect(durationInHours("10:30", "12:45")).toBeCloseTo(2.25);
+	expect(durationInHours("12:45", "14:30")).toBeCloseTo(1.75);
 });
